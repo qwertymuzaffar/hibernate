20ebf8ce82dd5113edc52262f71ff2c89388d5ce
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-07-27 17:06:02 -0400

    Setting Up Hibernate

--- src/main/java/com/hibernate/HibernateApplication.java
+++ src/main/java/com/hibernate/HibernateApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HibernateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HibernateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateApplication.class, args);
+    }
 
 }
